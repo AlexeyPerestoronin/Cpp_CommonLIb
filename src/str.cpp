7dac03294596b8b085c194b2cf5e97f88c4a0af6
Alexey Perestoronin <Alexey.Perestoronin@acronis.com>
2021-05-24 10:53:29 +0300

    micro refactoring of Str class

--- src/str.cpp
+++ src/str.cpp
@@ -1,15 +1,24 @@
 #include "str.hpp"
+
 #include "errors/base_error.hpp"
+
+
+#define RAISE_STR_ERROR(what_happend) \
+  RAISE_ERROR("common.lib:Str", what_happend);
+
 
 namespace Common
 {
   Str& Str::Replace(const Str& from, const Str& to)
   {
     if (to.find(from) != std::string::npos)
-      RAISE_ERROR("common.lib", "cannot perform recursive replacing");
+      RAISE_STR_ERROR("cannot perform recursive replacing");
 
     for (size_t from_size = from.size(), start = BaseType::find(from); start != std::string::npos; start = BaseType::find(from))
       BaseType::replace(start, from_size, std::move(to));
     return *this;
   }
 } // namespace Common
+
+
+#undef RAISE_STR_ERROR
